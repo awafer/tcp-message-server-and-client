29e584d0639405f8c97aec0597fdd71e2cb0bfcc
mugmickey <mugmickey@gmail.com>
2021-09-19 06:15:39 +0800

    minor changes.

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,10 +9,14 @@ namespace Client
 {
     class Program
     {
+        private static int _y;
+        private static Object _lock = new Object();
+
         static void Main(string[] args)
         {
             string serverip = "10.0.0.100";
             int serverport = 8001;
+
             try
             {
                 TcpClient tcpclnt;
@@ -21,21 +25,71 @@ namespace Client
                 tcpclnt = new TcpClient();
                 tcpclnt.Connect(serverip,serverport);
                 // use the ipaddress as in the server program
+                
+                Stream stm;
+                string str="start";
             
+                Thread worker = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        lock (str)
+                        {
+                            if (str.Equals("bye"))
+                            {
+                                break;
+                            }
+                        }
+                        stm = tcpclnt.GetStream();
+                        byte[] bb = new byte[100];
+                        int k; 
+                        
+                        k = stm.Read(bb, 0, 100);
+                        _y = Console.CursorTop;
+                            
+                        if (_y == Console.BufferHeight - 1)
+                        { 
+                            Console.WriteLine(); Console.SetCursorPosition(0, --_y);
+                        }
+                            
+                        int x = Console.CursorLeft;
+                        Console.MoveBufferArea(0, _y, Console.WindowWidth, 1, 0, _y + 1);
+                        //move the current line to next line, if the console start a new line, will move only the line at the bottom.
+                        Console.SetCursorPosition(0, _y);//back to previous line
+                            
+                        for (int i = 0; i < k; i++) 
+                        { 
+                            Console.Write(Convert.ToChar(bb[i]));
+                        }
+                            
+                        Console.SetCursorPosition(x, _y + 1);//move the cursor to where it was
+                    }
+                });
+                
+                worker.Start();
+                
                 Console.WriteLine("Connected");
                 while (true)
                 {
-                    Console.Write("Enter the string to be transmitted : ");
-                    string str = Console.ReadLine();
-                    Stream stm = tcpclnt.GetStream();
+                    Console.Write(">>>");
+                    str = Console.ReadLine();
+                    if (str.Length==0)
+                        continue;
+                    stm = tcpclnt.GetStream();
                     ASCIIEncoding asen = new ASCIIEncoding();
                     byte[] ba = asen.GetBytes(str);
+
                     Console.WriteLine("Transmitting.....");
                     stm.Write(ba, 0, ba.Length);
-                    byte[] bb = new byte[100];
-                    int k = stm.Read(bb, 0, 100);
-                    for (int i = 0; i < k; i++)
-                        Console.Write(Convert.ToChar(bb[i]));
+
+                    if (str.Equals("bye"))
+                    {
+                        break;
+                    }
+                    //byte[] bb = new byte[100];
+                    //int k = stm.Read(bb, 0, 100);
+                    //for (int i = 0; i < k; i++)
+                    //    Console.Write(Convert.ToChar(bb[i]));
                 }
                 tcpclnt.Close();
             }
