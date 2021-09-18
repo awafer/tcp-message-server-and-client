b55237a824cd032862e80514bf245475ca2e9545
K <keilapwong@gmail.com>
2021-09-18 18:17:38 +0800

    simple send and receive function.

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverip = "192.168.1.100";
+            int serverport = 8001;
+            try
+            {
+                TcpClient tcpclnt;
+                Console.WriteLine("Connecting.....");
+            
+                
+                // use the ipaddress as in the server program
+            
+                Console.WriteLine("Connected");
+                while (true)
+                {
+                    tcpclnt = new TcpClient();
+                    tcpclnt.Connect(serverip,serverport);
+                    Console.Write("Enter the string to be transmitted : ");
+                    string str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitting.....");
+                    stm.Write(ba, 0, ba.Length);
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(bb[i]));
+                    tcpclnt.Close();
+                }
+                
+            }
+        
+            catch (Exception e) {
+                Console.WriteLine("Error..... \n" + e.StackTrace);
+            }            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            /*   Server Program    */
+            string serverip = "192.168.1.100";
+            int serverport = 8001;
+            try {
+                IPAddress ipAd = IPAddress.Parse(serverip);
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener listenerlist=new TcpListener(ipAd,serverport);
+
+                /* Start Listening at the specified port */
+                
+                Console.WriteLine("The server is running at port "+serverport+"...");
+                Console.WriteLine("The local End point is " +
+                                  listenerlist.LocalEndpoint );
+                Console.WriteLine("Waiting for a connection...");
+                Socket skt;
+                byte[] b;
+                int k;
+                
+                listenerlist.Start();
+                skt =listenerlist.AcceptSocket();
+                while (true)
+                {
+                    b= new byte[100];
+                    k = skt.Receive(b);
+                    Console.WriteLine("Connection accepted from " + skt.RemoteEndPoint);
+                    Console.WriteLine("Recieved...");
+                    
+                    for (int i = 0; i < k; i++)
+
+                        Console.Write(Convert.ToChar(b[i]));
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+
+                    skt.Send(asen.GetBytes("The string was recieved by the server.\n"));
+
+                    Console.WriteLine("\nSent Acknowledgement");
+                    
+                    /* clean up */
+                    skt.Close();
+                    listenerlist.Stop();
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+}
